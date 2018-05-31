b194a4920cd764f0e0e06611d58620b6d7970bce
GBTC441002UR <GBTC441002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-05-31 15:24:28 -0400

    Initial commit

---
+++ src/com/company/DebugProg.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class DebugProg {
+ public static  void main(String[]  args) {
+   double   x, y;
+
+     x = 3.145;
+     y = 3.64;
+
+     System.out.println("pi  is aproximately " + x);
+     System.out.println("my GPA was  " +  y);
+ }
+}
